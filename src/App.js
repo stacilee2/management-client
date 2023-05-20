214902379eaf9c1981c81e024d23cbbc59be1901
Staci Rosales <stacileep2@gmail.com>
2023-05-20 18:07:35 -0500

    add content, styling to Home

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Caseload from './Caseload';
 import Locations from './Locations';
 import NavBar from './NavBar';
 import {GiConversation} from "react-icons/gi";
+import {AiOutlineMail} from "react-icons/ai";
+import { VscAccount } from "react-icons/vsc";
 
 function App() {
   return (
     <div className="App">
       <NavBar GiConversation={GiConversation} />
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Home VscAccount={VscAccount} AiOutlineMail={AiOutlineMail} />} />
         <Route path='/caseload' element={<Caseload />} />
         <Route path='/locations' element={<Locations />} />
       </Routes>
