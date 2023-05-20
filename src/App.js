1a93bf67005a24389c78f02cfa8ee30447acccb0
Staci Rosales <stacileep2@gmail.com>
2023-05-20 15:29:06 -0500

    Created components Home, Caseload, Locations, NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home';
+import Caseload from './Caseload';
+import Locations from './Locations';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Home />
+      <Caseload />
+      <Locations />
     </div>
   );
 }
---
+++ src/Caseload.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Caseload() {
+  return (
+    <div>Caseload</div>
+  )
+}
+
+export default Caseload;
